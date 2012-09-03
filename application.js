fa87c40fdac40a56cab68b998df35739676121b3
Bruno Evangelista <Bruno Evangelista@.(none)>
2012-09-03 13:52:31 -0700

    Reading and rendering Crytek sponza model

---
+++ application.js
@@ -0,0 +1,359 @@
+// Using namespaces
+window.vec3 = efw.vec3;
+window.mat4 = efw.mat4;
+
+// Shaders
+//var gVS_Simple = "attribute vec3 aPosition; attribute vec3 aNormal; attribute vec2 aUv0; varying vec4 oColor; void main(void) { gl_Position = vec4(aPosition, 1.0); oColor = vec4(0,0,0,0); }';
+//var gFS_Simple = 'precision mediump float; varying vec4 oColor; void main(void) { gl_FragColor = oColor; }';
+
+var gVS_Phong = 
+"uniform mat4 gMatW;"
++"uniform mat3 gMatWI;"
++"uniform mat4 gMatWVP;"
++"attribute vec3 aPosition;"
++"attribute vec3 aNormal;"
++"attribute vec2 aUv0;"
++"varying vec3 oWorldPosition;"
++"varying vec3 oWorldNormalVec;"
++"varying vec2 oUv0;"
++""
++"void main()"
++"{"
++"	vec4 position = vec4(aPosition, 1.0);\n"
++"	oWorldPosition = (position * gMatW).xyz;\n"
++"	oWorldNormalVec = gMatWI * aNormal;\n"
++"	oUv0 = aUv0;\n"
++"	gl_Position = position * gMatWVP;\n"
++"}"
++"";
+
+var gFS_Phong = 
+"precision mediump float;"
++"uniform vec3 gWorldEyePosition;"
++"uniform vec3 gWorldLight0Position;"
++"varying vec3 oWorldPosition;"
++"varying vec3 oWorldNormalVec;"
++"varying vec2 oUv0;"
++""
++"void main()"
++"{"
++"  vec3 worldPosition = oWorldPosition;\n"
++"	vec3 worldNormalVec = normalize(oWorldNormalVec);\n"
++"	vec3 worldEyeVec = normalize(gWorldEyePosition - worldPosition);\n"
++"	vec3 worldLight0Vec = normalize(gWorldLight0Position - worldPosition);\n"
++"	vec3 halfLightEyeVec = normalize(worldEyeVec + worldLight0Vec);\n"
++""	   
++"	float lightAttnCoeff = max(dot(worldNormalVec, worldLight0Vec), 0.0);\n"
++"	float specularIntensity = max(dot(worldNormalVec, halfLightEyeVec), 0.0);\n"
++"	vec4 surfaceColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
++"	vec4 lightColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
++""
+//+"	gl_FragColor = lightAttnCoeff * lightColor * surfaceColor + lightColor * lightAttnCoeff * specularIntensity;"
+//+"	gl_FragColor = lightAttnCoeff * lightColor * surfaceColor;"
++"	lightAttnCoeff = max(1.5*lightAttnCoeff + pow(specularIntensity, 8.0), 0.1);"
++"	gl_FragColor = vec4(lightAttnCoeff, lightAttnCoeff, lightAttnCoeff, 1);"
++"}"
++"";
+
+// Buffers
+var gShaderProgram;
+var gUniformMatW, gUniformMatWI, gUniformMatWVP;
+var gUniformEyePos, gUniformLight0Pos;
+var gAttr0, gAttr1, gAttr2;
+
+// Content
+var gAsyncLoading = 0;
+var gMeshes;
+var gMeshesRawData;
+var gMeshesGL = [];
+
+// Camera and Light
+//var gLightPosition = [1000, -10000, 500];
+var gLightPosition = [0, 1000.0, 0];
+var gCamera = {};
+gCamera.eyePosition = [-500.0, -800.0, 0.0];
+gCamera.lookAtPosition = vec3.add(gCamera.eyePosition, [1.0, 0.8, 0.0]);
+gCamera.upVector = [0.0, 1.0, 0.0];
+
+// Events
+var gIsMousePressed = [false, false, false];
+var gMouseLastPosition = [-1, -1];
+var gMousePositionDelta = [0, 0];
+
+// Timer
+var gPreviousTime = 0;
+var gElapsedTime = 0;
+
+
+function loadFileAsync(fullFilePath, fileType, functionPtr)
+{
+	gAsyncLoading++;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', fullFilePath, true);
+	xhr.responseType = fileType;
+
+	xhr.onload = function(e) {
+		gAsyncLoading--;
+		functionPtr(this.response);
+
+		//console.log(e);
+		//console.log(this);
+	};
+	
+	xhr.onerror = xhr.onabort = function(e) { gAsyncLoading--; }
+	xhr.send();
+}
+
+
+function loadContentAsync()
+{
+	// Load meshes and shaders
+	loadFileAsync('sponza.evd', 'text', function(data) { gMeshes = JSON.parse(data); } );
+	loadFileAsync('sponza.evb', 'arraybuffer', function(data) { gMeshesRawData = data; } );
+}
+
+
+function setDefaultRenderStates()
+{
+	gl.clearColor(1.0, 1.0, 0.0, 1.0);
+	gl.clearDepth(1.0);
+	
+	gl.enable(gl.DEPTH_TEST);
+}
+
+
+function initializeContent()
+{
+	setDefaultRenderStates();
+	
+	//console.log(gMeshes);
+	//console.log(gMeshesRawData);
+	gMeshesGL = new Array(gMeshes.length);
+	
+	var dataIndex = 0;
+	for (i=0; i<gMeshes.length; i++)
+	{
+		var mesh = gMeshes[i];
+		//console.log(mesh);
+		
+		var dataSize;
+		dataSize = mesh.vertexCount * (3+3+2) * 4;
+		dataIndex = ((dataIndex + 3) & ~3);
+		var vertices = new Float32Array(gMeshesRawData, dataIndex, (3+3+2)*mesh.vertexCount);
+		dataIndex += dataSize;
+		
+		dataSize = mesh.indexCount * 2;
+		var indices = new Uint16Array(gMeshesRawData, dataIndex, mesh.indexCount);
+		dataIndex += dataSize;
+
+		// Debug		
+		//console.log(vertices);
+		//console.log(indices);
+		
+		var vertexBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+		gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+		var indexBuffer = gl.createBuffer();
+   		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+		gMeshesGL[i] = { vertexBuffer : vertexBuffer, indexBuffer : indexBuffer, indexCount : mesh.indexCount };
+		
+		//
+		//break;
+	}
+	
+	// Release data
+	gMeshesRawData = null;
+	
+	var vertexShader = compileShader(gVS_Phong, gl.VERTEX_SHADER);
+	var fragmentShader = compileShader(gFS_Phong, gl.FRAGMENT_SHADER);
+	gShaderProgram = linkShader(vertexShader, fragmentShader);
+	gl.useProgram(gShaderProgram);
+	
+	gUniformMatW = gl.getUniformLocation(gShaderProgram, "gMatW");
+	gUniformMatWI = gl.getUniformLocation(gShaderProgram, "gMatWI");
+	gUniformMatWVP = gl.getUniformLocation(gShaderProgram, "gMatWVP");
+	gUniformEyePos = gl.getUniformLocation(gShaderProgram, "gWorldEyePosition");
+	gUniformLight0Pos = gl.getUniformLocation(gShaderProgram, "gWorldLight0Position");
+	
+	gAttr0 = gl.getAttribLocation(gShaderProgram, "aPosition");
+	gAttr1 = gl.getAttribLocation(gShaderProgram, "aNormal");
+	gAttr2 = gl.getAttribLocation(gShaderProgram, "aUv0");
+	gl.enableVertexAttribArray(gAttr0);
+	gl.enableVertexAttribArray(gAttr1);
+	gl.enableVertexAttribArray(gAttr2);
+
+	// Debug matrices
+	//console.log("Max GL Attribs: " + gl.getParameter(gl.MAX_VERTEX_ATTRIBS));
+	//console.log( matWorld );
+	//console.log( matWorldI );
+	//console.log( matView );
+	//console.log( matPerspective );
+	//console.log( matWVP );
+}
+
+
+function update(elapsedTimeMillis)
+{
+	// TODO Everything needs to be scaled according to the bounding sphere radius of the object
+	
+	// Update camera
+	if (gIsMousePressed[0])
+	{
+		var deltaX = gMousePositionDelta[0] * elapsedTimeMillis * 0.3;
+		var sinAngX = Math.sin(deltaX);
+		var cosAngX = Math.cos(deltaX);
+		//gCamera.eyePosition[1] += gMousePositionDelta[1] * elapsedTimeMillis*32;
+		//gCamera.eyePosition[0] += gMousePositionDelta[0] * elapsedTimeMillis*32;
+		
+		// Rotate lookAtPosition around up vector
+		var lookAtVec = vec3.sub(gCamera.lookAtPosition, gCamera.eyePosition);  
+		gCamera.lookAtPosition[0] = vec3.dot(lookAtVec, vec3.create(cosAngX, 0, sinAngX));
+		gCamera.lookAtPosition[1] = lookAtVec[1]; 
+		gCamera.lookAtPosition[2] = vec3.dot(lookAtVec, vec3.create(-sinAngX, 0, cosAngX));
+		gCamera.lookAtPosition = vec3.add(gCamera.lookAtPosition, gCamera.eyePosition);
+		
+		// Rotate lookAtPosition around right vector
+		var deltaY = gMousePositionDelta[1] * elapsedTimeMillis * 0.3;
+		var sinAngY = Math.sin(deltaY);
+		var cosAngY = Math.cos(deltaY);
+		
+		// Optimize using 3x3 matrices
+		var normalizedLookAtVec = vec3.normalize(lookAtVec);
+		var rightVec = vec3.normalize(vec3.cross(gCamera.upVector, normalizedLookAtVec));
+		var cameraBasis = mat4.createFromVec3(rightVec, gCamera.upVector, normalizedLookAtVec);
+		var invCameraBasis = mat4.transpose(cameraBasis);
+		var rotateX = mat4.rotateX(deltaY);
+		
+		//var rotateOverRight = mat4.mul(mat4.mul(invCameraBasis, rotateX), cameraBasis);
+		//gCamera.upVector = vec3.mulMat4(gCamera.upVector, rotateOverRight);
+		//gCamera.upVector = vec3.mulMat4(gCamera.upVector, rotateOverRight);
+		
+		//cancelRequestAnimFrame(gAnimationFrameRequest);
+		//while(true) {}
+	}
+	if (gMouseWheelDelta != 0)
+	{
+		var scaledLookAtVec = vec3.mulScalar(normalizedLookAtVec, gMouseWheelDelta*0.1);
+		
+		gCamera.eyePosition = vec3.add(gCamera.eyePosition, scaledLookAtVec);
+		gCamera.lookAtPosition = vec3.add(gCamera.lookAtPosition, scaledLookAtVec);
+	}
+	
+	//var matWorld = mat4.identity();
+	var matWorld = mat4.scale( vec3.create(1.0, 1.0, 1.0) );
+	var matWorldI = mat4.scale( vec3.create(1.0, 1.0, 1.0) );;
+	var matView = mat4.lookAtRH(gCamera.eyePosition, gCamera.lookAtPosition, gCamera.upVector);
+	var matPerspective = mat4.perspectiveFovRH(Math.PI*0.25, gl.viewportWidth/gl.viewportHeight, 0.1, 10000.0);
+	var matWVP = mat4.mul(matWorld, mat4.mul(matView, matPerspective));
+
+	gl.uniformMatrix4fv(gUniformMatW, false, new Float32Array(matWorld));
+	gl.uniformMatrix3fv(gUniformMatWI, false, new Float32Array( mat4.upper3x3(matWorldI) ));
+	gl.uniformMatrix4fv(gUniformMatWVP, false, new Float32Array(matWVP));
+	gl.uniform3fv(gUniformEyePos, new Float32Array(gCamera.eyePosition) );
+	gl.uniform3fv(gUniformLight0Pos, new Float32Array(gLightPosition) );
+}
+
+
+function draw(elapsedTimeMillis)
+{
+	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	for (var i=0; i<gMeshesGL.length; i++)
+	{
+		// Remove rendering of flag
+		if (i == 3)
+			continue;
+			
+		var meshGL = gMeshesGL[i];
+		
+		gl.bindBuffer(gl.ARRAY_BUFFER, meshGL.vertexBuffer);
+		gl.vertexAttribPointer(gAttr0, 3, gl.FLOAT, false, 32, 0);
+		gl.vertexAttribPointer(gAttr1, 3, gl.FLOAT, false, 32, 12);
+		gl.vertexAttribPointer(gAttr2, 2, gl.FLOAT, false, 32, 24);
+	
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, meshGL.indexBuffer);
+		gl.drawElements(gl.TRIANGLES, meshGL.indexCount, gl.UNSIGNED_SHORT, 0);
+	}
+}
+
+function onMouseMove(e) {
+	var mousePosition = [e.clientX, e.clientY];
+	gMousePositionDelta = [ mousePosition[0] - gMouseLastPosition[0], mousePosition[1] - gMouseLastPosition[1] ];
+	gMouseLastPosition = mousePosition;
+}
+function onMouseWheel(e) {
+	gMouseWheelDelta = e.wheelDelta;
+}
+
+function startApplication(canvas)
+{
+	initializeGL(canvas);
+	
+	loadContentAsync();
+	
+	//
+	canvas.addEventListener('mouseout', function(e) { gIsMousePressed = [false, false, false]; }, false);
+	canvas.addEventListener('mousedown', function(e) { gIsMousePressed[e.button] = true; gMouseLastPosition = [e.clientX, e.clientY]; /*console.log(e)*/ }, false);
+	canvas.addEventListener('mouseup', function(e) { gIsMousePressed[e.button] = false; /*console.log(e)*/ }, false);
+	canvas.addEventListener('mousemove', onMouseMove, false);
+	canvas.addEventListener('mousewheel', onMouseWheel, false);
+	
+	_waitForContent(_initialize);
+}
+
+function _waitForContent(functionPtr)
+{
+	if (gAsyncLoading == 0)
+		functionPtr();
+	else
+		setTimeout(function() { _waitForContent(functionPtr); }, 1000);
+}
+
+function _initialize()
+{
+	initializeContent();
+	
+	// Must set gPreviousTime before main loop starts
+	gPreviousTime = new Date().getTime();
+	requestAnimFrame(_mainLoop);
+}
+
+var gAnimationFrameRequest;
+function _mainLoop()
+{
+	gAnimationFrameRequest = requestAnimFrame(_mainLoop);
+	
+    var kMaxIterations = 60;
+    //var kDesiredElapsedTime = 1000/60.0; 
+    var kDesiredElapsedTime = 16;
+
+	var currentTime = new Date().getTime();
+	//gElapsedTime += Math.max(currentTime - gPreviousTime, 0);
+	gElapsedTime += currentTime - gPreviousTime;
+	gPreviousTime = currentTime;
+
+	//console.log(gElapsedTime);
+	//console.log(kDesiredElapsedTime);
+	
+	// May update multiple times if many frames have passed
+	var i = 0;
+    while (gElapsedTime >= kDesiredElapsedTime && i < kMaxIterations)
+    {
+    	gElapsedTime -= kDesiredElapsedTime;
+		update(kDesiredElapsedTime*0.001);
+		i++;
+    }
+    
+    // Reset any input delta
+    gMousePositionDelta = [0, 0];
+    gMouseWheelDelta = 0;
+    
+    // Only render if the scene was updated and we are not more than 8 frames behind
+    if (i >= 1 && i <= 8)
+		draw(kDesiredElapsedTime*0.001);
+}
---
+++ browsers.js
@@ -0,0 +1,74 @@
+// Global variable for OpenGL
+var gl;
+
+/* http://paulirish.com/2011/requestanimationframe-for-smart-animating/ */
+window.requestAnimFrame = ( function() {
+	return window.requestAnimationFrame 
+		|| window.webkitRequestAnimationFrame
+		|| window.mozRequestAnimationFrame
+		|| window.oRequestAnimationFrame
+		|| window.msRequestAnimationFrame
+		|| function(callback) { window.setTimeout(callback, 1000.0/60.0); }
+})();
+
+
+function initializeGL(canvas)
+{
+	try {
+		gl = canvas.getContext("webgl")
+			|| canvas.getContext("experimental-webgl");
+	} 
+	catch (e) { 
+		alert("Your browser don't support WebGL!");
+	}
+	
+	if (gl) {
+		//gl = WebGLDebugUtils.makeDebugContext(gl);
+		gl.viewportWidth = canvas.width;
+		gl.viewportHeight = canvas.height;
+	}
+}
+
+
+function compileShader(shaderSource, shaderType)
+{
+	var shader = gl.createShader(shaderType);
+	if (!shader) return null;
+	
+	gl.shaderSource(shader, shaderSource);
+	gl.compileShader(shader);
+	
+	if (gl.getShaderParameter(shader, gl.COMPILE_STATUS) == false)
+	{
+		gl.deleteShader(shader);
+		shader = null;
+
+		console.log("Shader source:\n" + shaderSource);
+		console.error("Error compiling shader:\n" + gl.getShaderInfoLog(shader));
+		
+		// TODO log error!
+	}
+		
+	return shader;
+}
+
+
+function linkShader(vertexShader, fragmentShader)
+{
+	var program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	
+	if (gl.getProgramParameter(program, gl.LINK_STATUS) == false)
+	{
+		gl.deleteProgram(program);
+		program = null;
+
+		console.error("Error linking shader programs: " + vertexShader + " and " + fragmentShader);
+	
+		// TODO Log error!
+	}
+	
+	return program;
+}
