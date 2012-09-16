fdae2354655c432b13407484fd24941488c4b8c0
Bruno Evangelista <>
2012-09-15 19:25:30 -0700

    Changes clear color Double buffering mouse input Improved mouse input to make it faster and more precise Changed mousedown, to fix mouse input bug that happens with very low fps Handling mouse scrolling on Firefox Enabled triangle culling Loading texture materials Increasing camera FOV a little bit to give the impression of a wider scene Added sampler2D to shader

--- application.js
+++ application.js
@@ -30,6 +30,7 @@ var gVS_Phong =
 
 var gFS_Phong = 
 "precision mediump float;"
++"uniform sampler2D gAlbedo;"
 +"uniform vec3 gWorldEyePosition;"
 +"uniform vec3 gWorldLight0Position;"
 +""
@@ -47,14 +48,16 @@ var gFS_Phong =
 +""	   
 +"	float lightAttnCoeff = max(dot(worldNormalVec, worldLight0Vec), 0.0);\n"
 +"	float specularIntensity = max(dot(worldNormalVec, halfLightEyeVec), 0.0);\n"
-+"	vec4 surfaceColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
+//+"	vec4 surfaceColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
++"	vec4 surfaceColor = texture2D(gAlbedo, oUv0);"
++"	float temp = surfaceColor.r; surfaceColor.r = surfaceColor.b; surfaceColor.b = temp;"
 +"	vec4 lightColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
 +""
 //+"	gl_FragColor = lightAttnCoeff * lightColor * surfaceColor + lightColor * lightAttnCoeff * specularIntensity;"
 //+"	gl_FragColor = lightAttnCoeff * lightColor * surfaceColor;"
 //+"	lightAttnCoeff = max(lightAttnCoeff + lightAttnCoeff*pow(specularIntensity, 8.0), 0.05);"
 //
-+"	gl_FragColor = vec4(lightAttnCoeff, lightAttnCoeff, lightAttnCoeff, 1);"
++"	gl_FragColor = surfaceColor * vec4(lightAttnCoeff, lightAttnCoeff, lightAttnCoeff, 1);"
 //+"	worldNormalVec = worldNormalVec * 0.5 + vec3(0.5);"
 //+"	gl_FragColor = vec4(worldNormalVec.x, worldNormalVec.y, worldNormalVec.z, 1);"
 +"}"
@@ -72,6 +75,10 @@ var gMeshes;
 var gMeshesRawData;
 var gMeshesGL = [];
 
+var gMaterials;
+var gMaterialsRawData;
+var gMaterialsGL = [];
+
 // Camera and Light
 //var gLightPosition = [1000, -10000, 500];
 var gLightPosition = [0.0, -800.0, 0.0];
@@ -82,13 +89,20 @@ gCamera.upVec = [0.0, 1.0, 0.0];
 
 // Events
 var gMouse = {};
+var gLastMouse = {};
+var gMouseWrite = {};
 gMouse.isPressed = [false, false, false];
-gMouse.lastPosition = [-1, -1];
-gMouse.position = [-1, -1];
-gMouse.positionDelta = [0, 0];
-gMouse.wheelDelta = [0];
+gMouse.position = [0, 0];
+gMouse.wheelDelta = 0;
+gLastMouse.isPressed = [false, false, false];
+gLastMouse.position = [0, 0];
+gLastMouse.wheelDelta = 0;
+gMouseWrite.isPressed = [false, false, false];
+gMouseWrite.position = [0, 0];
+gMouseWrite.wheelDelta = 0;
 
 // Timer
+var gAnimationFrameRequest;
 var gPreviousTime = 0;
 var gElapsedTime = 0;
 
@@ -105,7 +119,6 @@ function loadFileAsync(fullFilePath, fileType, functionPtr)
 		gAsyncLoading--;
 		functionPtr(this.response);
 		//console.log(e);
-		//console.log(this);
 	};
 	
 	xhr.onerror = xhr.onabort = function(e) { gAsyncLoading--; }
@@ -116,16 +129,23 @@ function loadFileAsync(fullFilePath, fileType, functionPtr)
 function loadContentAsync()
 {
 	// Load meshes and shaders
-	loadFileAsync('sponza.evd', 'text', function(data) { gMeshes = JSON.parse(data); } );
-	loadFileAsync('sponza.evb', 'arraybuffer', function(data) { gMeshesRawData = data; } );
+	loadFileAsync('sponza-meshes.evd', 'text', function(data) { gMeshes = JSON.parse(data); } );
+	loadFileAsync('sponza-materials.evd', 'text', function(data) { gMaterials = JSON.parse(data); } );
+	loadFileAsync('sponza-meshes.evb', 'arraybuffer', function(data) { gMeshesRawData = data; } );
+	loadFileAsync('sponza-materials.evb', 'arraybuffer', function(data) { gMaterialsRawData = data; } );
 }
 
 
 function setDefaultRenderStates()
 {
-	gl.clearColor(1.0, 1.0, 0.0, 1.0);
+	gl.clearColor(122/255.0, 170/255.0, 255/255.0, 1.0);
 	gl.clearDepth(1.0);
 	gl.enable(gl.DEPTH_TEST);
+	
+	gl.enable(gl.CULL_FACE);
+	gl.cullFace(gl.FRONT);
+	
+	gl.activeTexture(gl.TEXTURE0);
 }
 
 
@@ -133,6 +153,37 @@ function initializeContent()
 {
 	setDefaultRenderStates();
 	
+	// ------------------------------------------------------------
+	//console.log(gMaterials);
+	gMaterialsGL = new Array(gMaterials.length);
+	
+	var dataIndex = 0;
+	for (i=0; i<gMaterials.length; i++)
+	{
+		var material = gMaterials[i];
+		var albedoTexture = material.albedoTexture;
+		if (albedoTexture == null)
+			continue;
+		
+		//dataIndex = ((dataIndex + 3) & ~3);
+		var textureSize = Math.max(albedoTexture.width * albedoTexture.height * 4, 8);
+		var textureData = new Uint8Array(gMaterialsRawData, dataIndex, textureSize);
+		dataIndex += textureSize;
+		
+		var textureBuffer = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, textureBuffer);
+		//gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, albedoTexture.width, albedoTexture.height, 0, gl.RGBA, 
+			gl.UNSIGNED_BYTE, textureData);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    	
+    	gMaterialsGL[i] = {albedoTexture:textureBuffer};
+   	}
+	gMaterials = null;
+	gMaterialsRawData = null;
+	
+	// ------------------------------------------------------------
 	//console.log(gMeshes);
 	//console.log(gMeshesRawData);
 	gMeshesGL = new Array(gMeshes.length);
@@ -165,14 +216,14 @@ function initializeContent()
    		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
 		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
-		gMeshesGL[i] = { vertexBuffer : vertexBuffer, indexBuffer : indexBuffer, indexCount : mesh.indexCount };
+		gMeshesGL[i] = { materialId:mesh.materialId, vertexBuffer:vertexBuffer, indexBuffer:indexBuffer, indexCount:mesh.indexCount };
 		
 		//
 		//break;
 	}
-	
 	// Release data
 	gMeshesRawData = null;
+	gMeshes = null;
 	
 	var vertexShader = compileShader(gVS_Phong, gl.VERTEX_SHADER);
 	var fragmentShader = compileShader(gFS_Phong, gl.FRAGMENT_SHADER);
@@ -184,6 +235,7 @@ function initializeContent()
 	gUniformMatWVP = gl.getUniformLocation(gShaderProgram, "gMatWVP");
 	gUniformEyePos = gl.getUniformLocation(gShaderProgram, "gWorldEyePosition");
 	gUniformLight0Pos = gl.getUniformLocation(gShaderProgram, "gWorldLight0Position");
+	gUniformSamplerAlbedo = gl.getUniformLocation(gShaderProgram, "gAlbedo");
 	
 	gAttr0 = gl.getAttribLocation(gShaderProgram, "aPosition");
 	gAttr1 = gl.getAttribLocation(gShaderProgram, "aNormal");
@@ -205,11 +257,15 @@ function initializeContent()
 function update(elapsedTimeMillis)
 {
 	// TODO Everything needs to be scaled according to the bounding sphere radius of the object
-	
+	var positionDelta = [
+		gMouse.position[0] - gLastMouse.position[0],
+		gMouse.position[1] - gLastMouse.position[1]
+	];
+
 	// Update camera
 	if (gMouse.isPressed[0])
 	{
-		var deltaX = gMouse.positionDelta[0] * elapsedTimeMillis * 0.25;
+		var deltaX = positionDelta[0] * elapsedTimeMillis * 0.25;
 		var sinAngX = Math.sin(deltaX);
 		var cosAngX = Math.cos(deltaX);
 		
@@ -220,7 +276,7 @@ function update(elapsedTimeMillis)
 		gCamera.lookAtVec = vec3.normalize(gCamera.lookAtVec);
 		
 		// Rotate lookAtPosition around right vector
-		var deltaY = -gMouse.positionDelta[1] * elapsedTimeMillis * 0.15;
+		var deltaY = -positionDelta[1] * elapsedTimeMillis * 0.15;
 		var sinAngY = Math.sin(deltaY);
 		var cosAngY = Math.cos(deltaY);
 		
@@ -250,10 +306,10 @@ function update(elapsedTimeMillis)
 	}
 	
 	//var matWorld = mat4.identity();
-	var matWorld = mat4.scale( vec3.create(1.0, 1.0, 1.0) );
-	var matWorldIT = mat4.scale( vec3.create(1.0, -1.0, 1.0) );
+	var matWorld = mat4.scale( vec3.create(1.5, 1.0, 1.5) );
+	var matWorldIT = mat4.scale( vec3.create(1/1.5, -1.0, 1/1.5) );
 	var matView = mat4.lookAtRH(gCamera.eyePos, vec3.add(gCamera.eyePos, gCamera.lookAtVec), gCamera.upVec);
-	var matPerspective = mat4.perspectiveFovRH(Math.PI*0.25, gl.viewportWidth/gl.viewportHeight, 0.1, 10000.0);
+	var matPerspective = mat4.perspectiveFovRH(Math.PI*0.35, gl.viewportWidth/gl.viewportHeight, 1.0, 5000.0);
 	var matWVP = mat4.mul(matWorld, mat4.mul(matView, matPerspective));
 
 	gl.uniformMatrix4fv(gUniformMatW, false, new Float32Array(matWorld));
@@ -276,7 +332,15 @@ function draw(elapsedTimeMillis)
 			continue;
 			
 		var meshGL = gMeshesGL[i];
-		
+		var material = gMaterialsGL[meshGL.materialId];
+		//console.log(material);
+		//console.log(meshGL);
+		if (material)
+		{
+        	gl.bindTexture(gl.TEXTURE_2D, material.albedoTexture);
+        	gl.uniform1i(gUniformSamplerAlbedo, 0);
+		}
+
 		gl.bindBuffer(gl.ARRAY_BUFFER, meshGL.vertexBuffer);
 		gl.vertexAttribPointer(gAttr0, 3, gl.FLOAT, false, 32, 0);
 		gl.vertexAttribPointer(gAttr1, 3, gl.FLOAT, false, 32, 12);
@@ -287,14 +351,28 @@ function draw(elapsedTimeMillis)
 	}
 }
 
+function updateMouse() {
+	gLastMouse.isPressed = gMouse.isPressed;
+	gLastMouse.position = gMouse.position;
+	gLastMouse.wheelDelta = gMouse.wheelDelta;
+	
+	gMouse.isPressed = gMouseWrite.isPressed;
+	gMouse.position = gMouseWrite.position;
+	gMouse.wheelDelta = gMouseWrite.wheelDelta;
+	
+	gMouseWrite.wheelDelta = 0;
+}
+function onMouseOut(e) {
+	gMouseWrite.isPressed = [false, false, false];
+}
 function onMouseMove(e) {
-	gMouse.lastPosition = (gMouse.position[0] != -1)? gMouse.position : [e.clientX, e.clientY];
-	gMouse.position = [e.clientX, e.clientY];
-	gMouse.positionDelta[0] = gMouse.position[0] - gMouse.lastPosition[0];
-	gMouse.positionDelta[1] = gMouse.position[1] - gMouse.lastPosition[1];
+	gMouseWrite.position = [e.clientX, e.clientY];
 }
 function onMouseWheel(e) {
-	gMouse.wheelDelta = e.wheelDelta;
+	gMouseWrite.wheelDelta = e.wheelDelta;
+}
+function onMouseWheelFirefox(e) {
+	gMouseWrite.wheelDelta = e.detail*-120;
 }
 
 function startApplication(canvas)
@@ -304,11 +382,12 @@ function startApplication(canvas)
 	loadContentAsync();
 	
 	//
-	canvas.addEventListener('mouseout', function(e) { gMouse.isPressed = [false, false, false]; gMouse.position = [-1,-1]; gMouse.lastPosition = [-1, -1]; }, false);
-	canvas.addEventListener('mousedown', function(e) { gMouse.isPressed[e.button] = true; /*console.log(e)*/ }, false);
-	canvas.addEventListener('mouseup', function(e) { gMouse.isPressed[e.button] = false; /*console.log(e)*/ }, false);
+	canvas.addEventListener('mouseout', onMouseOut, false);
+	canvas.addEventListener('mousedown', function(e) { gMouseWrite.isPressed[e.button] = true; onMouseMove(e); updateMouse(); updateMouse(); /*console.log(e)*/ }, false);
+	canvas.addEventListener('mouseup', function(e) { gMouseWrite.isPressed[e.button] = false; /*console.log(e)*/ }, false);
 	canvas.addEventListener('mousemove', onMouseMove, false);
 	canvas.addEventListener('mousewheel', onMouseWheel, false);
+	canvas.addEventListener('DOMMouseScroll', onMouseWheelFirefox, false);
 	
 	_waitForContent(_initialize);
 }
@@ -323,11 +402,11 @@ function _waitForContent(functionPtr)
 
 function _initialize()
 {
-	// Remove loading message
 	var loadingDiv = document.getElementById("div-loading");			 
 	if (loadingDiv)
+	{
 		loadingDiv.parentNode.removeChild(loadingDiv);
-	
+	}
 	initializeContent();
 	
 	// Must set gPreviousTime before main loop starts
@@ -335,12 +414,12 @@ function _initialize()
 	requestAnimFrame(_mainLoop);
 }
 
-var gAnimationFrameRequest;
+
 function _mainLoop()
 {
 	gAnimationFrameRequest = requestAnimFrame(_mainLoop);
 	
-    var kMaxIterations = 8;
+    var kMaxIterations = 4;
     var kDesiredElapsedTime = 16;
     var kInvDesiredElapsedTime = 1.0/16;
 
@@ -348,30 +427,32 @@ function _mainLoop()
 	//gElapsedTime += Math.max(currentTime - gPreviousTime, 0);
 	gElapsedTime = currentTime - gPreviousTime;
 	gPreviousTime = currentTime;
-
 	//console.log(gElapsedTime);
-	//console.log(kDesiredElapsedTime);
 	
 	// May update multiple times if many frames have passed
 	var updateCount = Math.min( gElapsedTime*kInvDesiredElapsedTime, kMaxIterations);
-	//var invUpdateCount = 1.0/updateCount;
-	//gMouse.positionDelta[0] *= invUpdateCount;
-	//gMouse.positionDelta[1] *= invUpdateCount;
-	
+
+	//console.time("update-all")
 	var i;
 	for (i=0; i < updateCount; i++)
     {
+		updateMouse();
+
+    	//console.time("update")
 		update(kDesiredElapsedTime*0.001);
+		//console.timeEnd("update");
 		i++;
     }
+    //console.timeEnd("update-all");
+
     //gElapsedTime -= updateCount*kDesiredElapsedTime;
     //gElapsedTime = 0;
-    	    
-    // Reset any input delta
-    gMouse.positionDelta = [0, 0];
-    gMouse.wheelDelta = 0;
-    
+    	        
     // Only render if the scene was updated and we are not more than 8 frames behind
     if (i >= 1 && i <= 2)
+    {
+    	//console.time("draw");
 		draw(kDesiredElapsedTime*0.001);
+		//console.timeEnd("draw");
+	}
 }
