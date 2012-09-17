d64de79c7d37078228ba3546305400185a55ada4
Bruno Evangelista <>
2012-09-16 20:32:48 -0700

    Clean version of math library (no error checking).

--- efw-math.js
+++ efw-math.js
@@ -4,55 +4,23 @@ efw.vec4 = efw.vec4 || {};
 efw.mat4 = efw.mat4 || {};
 
 efw.kEpsilon = 1e-6;
-efw.isNumber = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
-efw.assert = function(expression) {
-	if ((expression) == false)
-	{
-		if (window.console)
-		{
-			if (window.console.error)
-				console.error("Assert failed!");
-			else if (window.console.log)
-				console.log("Assert failed!");
-		}
-		else
-		{
-			// TODO
-		}
-	}	
-}
 
-//efw.vec3.isVec3 = function(m){}
-efw.vec3.isVec3 = function(v) {
-	return (v && v.length == 3 &&
-		efw.isNumber(v[0]) &&
-		efw.isNumber(v[1]) &&
-		efw.isNumber(v[2])
-	);
-}
 efw.vec3.isUnit = function(v) {
-	efw.assert( efw.vec3.isVec3(v) );
 	return Math.abs(vec3.lengthSquared(v) - 1.0) < efw.kEpsilon;
 }
 efw.vec3.create = function(x, y, z) {
 	return [x, y, z];
 }
 efw.vec3.add = function(v1, v2) {
-	efw.assert( efw.vec3.isVec3(v1) && efw.vec3.isVec3(v2) );
 	return [v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2]];
 }
 efw.vec3.sub = function(v1, v2) {
-	efw.assert( efw.vec3.isVec3(v1) && efw.vec3.isVec3(v2) );
 	return [v1[0]-v2[0], v1[1]-v2[1], v1[2]-v2[2]];
 }
 efw.vec3.mul = function(v1, v2) {
-	efw.assert( efw.vec3.isVec3(v1) && efw.vec3.isVec3(v2) );
 	return [ v1[0]*v2[0], v1[1]*v2[1], v1[2]*v2[2] ];
 }
 efw.vec3.mulMat4 = function(v, m) {
-	efw.assert( efw.vec3.isVec3(v) && efw.mat4.isMat4(m) );
 	return [ 
 		v[0]*m[0]+v[1]*m[4]+v[2]*m[8],
 		v[0]*m[1]+v[1]*m[5]+v[2]*m[9],
@@ -60,28 +28,22 @@ efw.vec3.mulMat4 = function(v, m) {
 		];
 }
 efw.vec3.mulScalar = function(v, s) {
-	efw.assert( efw.vec3.isVec3(v) && efw.isNumber(s) );
 	return [ v[0]*s, v[1]*s, v[2]*s ];
 }
 efw.vec3.length = function(v) {
-	efw.assert( efw.vec3.isVec3(v) );
 	return Math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]);
 }
 efw.vec3.lengthSquared = function(v) {
-	efw.assert( efw.vec3.isVec3(v) );
 	return v[0]*v[0]+v[1]*v[1]+v[2]*v[2];
 }
 efw.vec3.normalize = function(v) {
-	efw.assert( efw.vec3.isVec3(v) );
 	var lengthInv = 1.0/efw.vec3.length(v);
 	return [v[0]*lengthInv, v[1]*lengthInv, v[2]*lengthInv];
 }
 efw.vec3.dot = function(v1, v2) {
-	efw.assert( efw.vec3.isVec3(v1) && efw.vec3.isVec3(v2) );
 	return (v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2]); 
 }
 efw.vec3.cross = function(v1, v2) {
-	efw.assert( efw.vec3.isVec3(v1) && efw.vec3.isVec3(v2) );
 	return [
 		v1[1]*v2[2] - v1[2]*v2[1],
 		v1[2]*v2[0] - v1[0]*v2[2],
@@ -89,36 +51,22 @@ efw.vec3.cross = function(v1, v2) {
 	];
 }
 
-//efw.vec4.isVec4 = function(m){}
-efw.vec4.isVec4 = function(v) {
-	return (v && v.length == 4 &&
-		efw.isNumber(v[0]) &&
-		efw.isNumber(v[1]) &&
-		efw.isNumber(v[2]) &&
-		efw.isNumber(v[3])
-	);
-}
 efw.vec4.isUnit = function(v) {
-	efw.assert( efw.vec4.isVec4(v) );
 	return Math.abs(vec4.lengthSquared(v) - 1.0) < efw.kEpsilon;
 }
 efw.vec4.create = function(x, y, z, w) {
 	return [x, y, z, w];
 }
 efw.vec4.add = function(v1, v2) {
-	efw.assert( efw.vec4.isVec4(v1) && efw.vec4.isVec4(v2) );
 	return [v1[0]+v2[0], v1[1]+v2[1], v1[2]+v2[2], v1[3]+v2[3]];
 }
 efw.vec4.sub = function(v1, v2) {
-	efw.assert( efw.vec4.isVec4(v1) && efw.vec4.isVec4(v2) );
 	return [v1[0]-v2[0], v1[1]-v2[1], v1[2]-v2[2], v1[3]-v2[3]];
 }
 efw.vec4.mul = function(v1, v2) {
-	efw.assert( efw.vec4.isVec4(v1) && efw.vec4.isVec4(v2) );
 	return [ v1[0]*v2[0], v1[1]*v2[1], v1[2]*v2[2], v1[3]*v2[3] ];
 }
 efw.vec4.mulMat4 = function(v, m) {
-	efw.assert( efw.vec4.isVec4(v) && efw.mat4.isMat4(m) );
 	return [ 
 		v[0]*m[0]+v[1]*m[4]+v[2]*m[8]+v[3]*m[12],
 		v[0]*m[1]+v[1]*m[5]+v[2]*m[9]+v[3]*m[13],
@@ -127,24 +75,19 @@ efw.vec4.mulMat4 = function(v, m) {
 		];
 }
 efw.vec4.mulScalar = function(v, s) {
-	efw.assert( efw.vec4.isVec4(v) && efw.isNumber(s) );
 	return [ v[0]*s, v[1]*s, v[2]*s, v[3]*s ];
 }
 efw.vec4.length = function(v) {
-	efw.assert( efw.vec4.isVec4(v) );
 	return Math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2]+v[3]*v[3]);
 }
 efw.vec4.lengthSquared = function(v) {
-	efw.assert( efw.vec4.isVec4(v) );
 	return v[0]*v[0]+v[1]*v[1]+v[2]*v[2]+v[3]*v[3];
 }
 efw.vec4.normalize = function(v) {
-	efw.assert( efw.vec4.isVec4(v) );
 	var lengthInv = 1.0/efw.vec4.length(v);
 	return [v[0]*lengthInv, v[1]*lengthInv, v[2]*lengthInv, v[3]*lengthInv];
 }
 efw.vec4.dot = function(v1, v2) {
-	efw.assert( efw.vec4.isVec4(v1) && efw.vec4.isVec4(v2) );
 	return (v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2]+v1[3]*v2[3]);
 }
 
@@ -206,7 +149,6 @@ efw.mat4.identity = function() {
 	];
 }
 efw.mat4.transpose = function(m) {
-	efw.assert( efw.mat4.isMat4(m) );
 	return [
 		m[0], m[4], m[8], m[12],  
 		m[1], m[5], m[9], m[13],
@@ -215,7 +157,6 @@ efw.mat4.transpose = function(m) {
 	];
 }
 efw.mat4.mul = function(m1, m2) {
-	efw.assert( efw.mat4.isMat4(m1) && efw.mat4.isMat4(m2) );
 	return [
 		m2[0] * m1[0] + m2[1] * m1[4] + m2[2] * m1[8] + m2[3] * m1[12],  
 		m2[0] * m1[1] + m2[1] * m1[5] + m2[2] * m1[9] + m2[3] * m1[13],
@@ -239,7 +180,6 @@ efw.mat4.mul = function(m1, m2) {
 	];
 }
 efw.mat4.upper3x3 = function(m) {
-	efw.assert( efw.mat4.isMat4(m) );
 	return [ 
 		m[0], m[1], m[2],
 		m[4], m[5], m[6],
@@ -247,7 +187,6 @@ efw.mat4.upper3x3 = function(m) {
 	];
 }
 efw.mat4.lookAtRH = function(eyePos, lookAtPos, upVec) {
-	efw.assert( efw.vec3.isVec3(eyePos) && efw.vec3.isVec3(lookAtPos) && efw.vec3.isVec3(upVec) );
 	var axisZ = efw.vec3.normalize( efw.vec3.sub(lookAtPos, eyePos) );
 	var axisX = efw.vec3.normalize( efw.vec3.cross(upVec, axisZ) );
 	var axisY = efw.vec3.cross(axisZ, axisX);
@@ -260,7 +199,6 @@ efw.mat4.lookAtRH = function(eyePos, lookAtPos, upVec) {
 	];
 }
 efw.mat4.perspectiveFovRH = function(fovY, aspectRatio, nearZ, farZ) {
-	efw.assert( efw.isNumber(fovY) && efw.isNumber(aspectRatio) && efw.isNumber(nearZ) && efw.isNumber(farZ) ); 
 	var scaleY = 1.0/Math.tan(fovY*0.5);
 	var scaleX = scaleY / aspectRatio;
 	var rangeZInv = 1.0/(farZ-nearZ);
@@ -272,7 +210,6 @@ efw.mat4.perspectiveFovRH = function(fovY, aspectRatio, nearZ, farZ) {
 	];
 }
 efw.mat4.rotateX = function(ang) {
-	efw.assert( efw.isNumber(ang) );
 	var sinAng = Math.sin(ang);
 	var cosAng = Math.cos(ang);
 	return [
@@ -283,7 +220,6 @@ efw.mat4.rotateX = function(ang) {
 	];
 }
 efw.mat4.rotateY = function(ang) {
-	efw.assert( efw.isNumber(ang) );
 	var sinAng = Math.sin(ang);
 	var cosAng = Math.cos(ang);
 	return [
@@ -294,7 +230,6 @@ efw.mat4.rotateY = function(ang) {
 	];
 }
 efw.mat4.rotateZ = function(ang) {
-	efw.assert( efw.isNumber(ang) );
 	var sinAng = Math.sin(ang);
 	var cosAng = Math.cos(ang);
 	return [
@@ -305,7 +240,6 @@ efw.mat4.rotateZ = function(ang) {
 	];	
 }
 efw.mat4.scale = function(s) {
-	efw.assert( efw.vec3.isVec3(s) );
 	return [
 		s[0], 0, 0, 0,
 		0, s[1], 0, 0,
@@ -314,7 +248,6 @@ efw.mat4.scale = function(s) {
 	];
 }
 efw.mat4.translate = function(t) {
-	efw.assert( efw.vec3.isVec3(t) );
 	return [
 		1, 0, 0, t[0],
 		0, 1, 0, t[1],
