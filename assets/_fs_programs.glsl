0eb5da092e1a60dbf6bce641fee9d675b03c9990
Bruno Evangelista <>
2012-10-29 01:37:52 -0700

    Trying to understand and improve physically based lighting

--- assets/_fs_programs.glsl
+++ assets/_fs_programs.glsl
@@ -4,11 +4,11 @@ uniform sampler2D gSamplerAlbedo;
 
 uniform vec3 gWorldEyePosition;
 uniform vec3 gLight0WorldPosition;
-uniform vec3 gLight0Color;
 uniform vec3 gLight1WorldPosition;
+uniform vec3 gLight0Color;
 uniform vec3 gLight1Color;
 
-uniform float gMaterialFresnel0;
+uniform vec3 gMaterialFresnel0;
 uniform float gMaterialRoughness;
 
 varying vec3 oWorldPosition;
@@ -25,46 +25,64 @@ float convertShininessToRoughness(float value)
 	return sqrt(2.0 / (value + 2.0));
 }
 
-float calculateF0(float n2)
+// Refractive index is per wavelength (opt pass 3 parameters)
+vec3 calculateF0(float n2)
 {
 	float result = (1.0 - n2) / (1.0 + n2);
 	result *= result;
-	return result;
+	return vec3(result);
 }
 
-float fresnel(float f0, float dotHV)
+vec3 fresnel(vec3 f0, float dotHV)
 {
 	// where dotHV is the cosine of the half angle between the incoming and outgoing light directions
-	return f0 + (1.0 - f0) * pow(1.0 - dotHV, 5.0);
+	return f0 + (vec3(1.0) - f0) * vec3(pow(1.0 - dotHV, 5.0));
 }
 
-vec3 gamma(vec3 input)
+vec3 gamma(vec3 value)
 {
-	return pow(input, 1.0/2.2);
+	return pow(value, vec3(1.0/2.2));
 }
 
-vec3 degamma(vec3 input)
+vec3 degamma(vec3 value)
 {
-	return pow(input, 2.2);
+	return pow(value, vec3(2.2));
 }
 
 vec3 blinnPhong(vec3 normalVec, vec3 eyeVec, vec3 lightVec, vec3 materialAlbedo, float materialShininess, vec3 lightColor)
 {
 	vec3 halfVec = normalize(eyeVec + lightVec);
 	
-	float dotNormalLight = max(dot(normalVec, normalVec), 0.0);
+	
+	float dotNormalLight = max(dot(normalVec, lightVec), 0.0);
 	float dotNormalHalf = max(dot(normalVec, halfVec), 0.0);
 	float dotEyeHalf = max(dot(eyeVec, halfVec), 0.0);
 	
 	// Specular
-	float specularFresnel = fresnel(gMaterialFresnel0, dotEyeHalf);
+	vec3 specularFresnel = fresnel(gMaterialFresnel0, dotEyeHalf);
 	//float specularNdf = ( pow(dotHalfNormal, gRoughness) + 2.0 ) * 0.5; 
-	float specularNormalizedNdf = pow(dotHalfNormal, materialShininess) * 0.5 + 1.0;
+	float oneOverPi = 1.0/3.141592;
+	float oneOver2Pi = 0.5/3.141592;
+	float specularNormalization = materialShininess * oneOver2Pi + oneOverPi;
+	//float specularNormalization = (materialShininess + 2.0) / (2.0 * 3.141592);
+	vec3 specularNormalizedNdf = vec3( pow(dotNormalHalf, materialShininess) * specularNormalization );
 	
 	// Diffuse
-	float diffuseFresnel = 1.0 - fresnel(gMaterialFresnel0, dotNormalLight);
+	//vec3 diffuseFresnel = vec3(1.0) - fresnel(gMaterialFresnel0, dotNormalLight);
+	//vec3 diffuseFresnel = specularFresnel;
 	
-	return ( diffuseFresnel * materialAlbedo + specularFresnel * specularNormalizedNdf ) * lightColor * vec3(dotNormalLight);
+	//vec3 diffuseFresnel = vec3(1.0) - fresnel( vec3(0.45), dotNormalLight);
+	
+	const float kInvPi = 0.5/3.141592;
+	//materialAlbedoColor *= kInvPi;
+
+	return ( materialAlbedo * kInvPi + materialAlbedo * specularFresnel * specularNormalizedNdf ) * lightColor * vec3(dotNormalLight);
+	
+	//return ( materialAlbedo + 0.7 * specularFresnel * specularNormalizedNdf ) * lightColor * vec3(dotNormalLight);
+	//return ( materialAlbedo + materialAlbedo * specularFresnel * specularNormalizedNdf ) * lightColor * vec3(dotNormalLight);
+	
+	//return specularNormalizedNdf * lightColor * vec3(dotNormalLight);
+	//return lightColor * vec3(dotNormalLight);
 }
 
 
@@ -75,19 +93,34 @@ void main()
 	vec3 worldNormalVec = normalize(oWorldNormalVec);
 	vec3 worldEyeVec = normalize(gWorldEyePosition - worldPosition);
 
-	vec3 worldLight0Vec = normalize(gLight0WorldPosition - worldPosition);
-	vec3 worldLight1Vec = normalize(gLight1WorldPosition - worldPosition);
-
+	vec3 worldLight0Vec = gLight0WorldPosition - worldPosition;
+	vec3 worldLight1Vec = gLight1WorldPosition - worldPosition;
+	float light0Dist = length(worldLight0Vec);
+	float light1Dist = length(worldLight1Vec);
+	worldLight0Vec *= 1.0 / light0Dist; 
+	worldLight1Vec *= 1.0 / light1Dist;
+	
+	//const float innerRange = 200.0;
+	const float outerRange = 1900.0;
+	//float light0Attn = 1.0 - clamp((light0Dist - innerRange) / outerRange, 0.0, 1.0); 
+	//float light1Attn = 1.0 - clamp((light1Dist - innerRange) / outerRange, 0.0, 1.0);
+	float light0Attn = 1.0 - clamp(light0Dist / outerRange, 0.0, 1.0);
+	float light1Attn = 1.0 - clamp(light1Dist / outerRange, 0.0, 1.0);
+	light0Attn *= light0Attn;
+	light1Attn *= light1Attn;
+	
 	// Material	
-	const float kInvPi = 0.31831;
 	vec3 materialAlbedoColor = degamma( texture2D(gSamplerAlbedo, oUv0).xyz );
-	materialAlbedoColor *= kInvPi; // 1.0 / PI
 	
 	// Lights
-	vec3 result0 = blinnPhong(worldNormalVec, worldEyeVec, worldLight0Vec, materialAlbedoColor, gMaterialRoughness, gLight0Color);
-	vec3 result1 = blinnPhong(worldNormalVec, worldEyeVec, worldLight1Vec, materialAlbedoColor, gMaterialRoughness, gLight1Color);
-	gl_FragColor = vec4(gamma( result0 + result1 ), 1.0);
+	vec3 result0 = blinnPhong(worldNormalVec, worldEyeVec, worldLight0Vec, materialAlbedoColor, gMaterialRoughness, gLight0Color * light0Attn);
+	vec3 result1 = blinnPhong(worldNormalVec, worldEyeVec, worldLight1Vec, materialAlbedoColor, gMaterialRoughness, gLight1Color * light1Attn);
+	gl_FragColor = vec4( gamma(result0 + result1), 1.0 );
 	
+	//gl_FragColor.xyz = gamma(result0);
+	//gl_FragColor.xyz = gamma( vec3( max(0.0, dot(worldNormalVec, worldLight0Vec)) ) );
+	//gl_FragColor.xyz = result0;
+	//gl_FragColor.w = 1.0;
 	/*
 	
 	vec3 halfLight0EyeVec = normalize(worldEyeVec + worldLight0Vec);
