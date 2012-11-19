eeb7d682d4a443bc9866ea21ecde7368272263c5
Bruno Evangelista <>
2012-11-18 20:40:06 -0800

    Fixing output

--- compile-app.bat
+++ compile-app.bat
@@ -1,3 +1,3 @@
 @setlocal enableextensions enabledelayedexpansion
 
-call java -jar .\closure\compiler.jar  --jscomp_off=checkVars --js application.js --externs efw.min.js --js_output_file .\dist\app.min.js 2>.\dist\build_application_output.txt
+call java -jar .\closure\compiler.jar  --jscomp_off=checkVars --js application.js --externs dist\efw.min.js --js_output_file .\dist\app.min.js 2>.\dist\build_application_output.txt
