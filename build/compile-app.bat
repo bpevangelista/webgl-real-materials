e326ef656019f65f2c58648d1c760f7b041a9ed9
Bruno Evangelista <>
2012-11-25 23:35:22 -0800

    Removing license from main app file

--- build/compile-app.bat
+++ build/compile-app.bat
@@ -10,6 +10,7 @@ call compile-release.bat
 @echo on
 call java -jar .\..\closure\compiler.jar --jscomp_off=externsValidation --js ..\application.js --externs ..\dist\efw.min.js --js_output_file .\..\dist\app.min-nolicense.js 2>build_application_output.txt
 
-copy /b /y ..\license.txt + ..\application.js ..\dist\app.all.js
+@REM copy /b /y ..\license.txt + ..\application.js ..\dist\app.all.js
+copy /b /y ..\application.js ..\dist\app.all.js
 copy /b /y ..\license.txt + ..\dist\app.min-nolicense.js ..\dist\app.min.js
 del /q ..\dist\app.min-nolicense.js
